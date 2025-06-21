d2eb36cadb70e74013657c96a90e871e357d08b2
Divyadarshini A <163431239+divyadarshini16@users.noreply.github.com>
2025-06-21 12:11:34 +0530

    Create CommandPattern.java

---
+++ CommandPattern.java
@@ -0,0 +1,79 @@
+
+interface Command {
+    void execute();
+}
+
+class Light {
+    public void turnOn() {
+        System.out.println("💡 Light is ON");
+    }
+
+    public void turnOff() {
+        System.out.println("💡 Light is OFF");
+    }
+}
+
+
+class LightOnCommand implements Command {
+    private Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.turnOn();
+    }
+}
+
+
+class LightOffCommand implements Command {
+    private Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.turnOff();
+    }
+}
+
+
+class RemoteControl {
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void pressButton() {
+        if (command != null) {
+            command.execute();
+        } else {
+            System.out.println("No command set.");
+        }
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Light livingRoomLight = new Light();
+
+      
+        Command lightOn = new LightOnCommand(livingRoomLight);
+        Command lightOff = new LightOffCommand(livingRoomLight);
+
+      
+        RemoteControl remote = new RemoteControl();
+
+       
+        remote.setCommand(lightOn);
+        remote.pressButton();
+
+       
+        remote.setCommand(lightOff);
+        remote.pressButton();
+    }
+}
